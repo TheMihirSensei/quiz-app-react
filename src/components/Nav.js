696de09efe7ec633a8f4e709eac0729b3da97fc1
MihirSensei <mihirsidhdhapura41@gmail.com>
2022-01-20 00:24:44 +0530

    navbar initial-commit

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Nav.css";
+
+function Nav() {
+  return <div className="nav"> 
+    <div className='navItem'>Otaku Family</div>
+    <div className='navItem'>Logo</div>
+    <div className='navItem'>User</div>
+  </div>;
+}
+
+export default Nav;
