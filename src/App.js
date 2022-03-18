2646efec980e13ee58e01d30790a142d2eb4faf3
Mihir Sidhdhapura <mihir.s@squareboat.com>
2022-03-19 00:26:23 +0530

    CHANGE: api called

--- src/App.js
+++ src/App.js
@@ -4,87 +4,101 @@ import Nav from "./components/Nav";
 import Qna from "./components/Qna";
 import { Swiper, SwiperSlide } from "swiper/react";
 // import { EffectFade } from 'swiper';
-import 'swiper/css';
+import "swiper/css";
 // import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import mhaQna from "./asserts/mhaQna.json"
-import { Navigation, Pagination, Scrollbar, A11y, EffectCards, EffectFade } from 'swiper';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import mhaQna from "./asserts/mhaQna.json";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCards,
+  EffectFade,
+} from "swiper";
 import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function App() {
-
-
   const clicked = () => {
     console.log("clicked in app");
   };
+  const [quiz, setQuiz] = useState()
+  const [qa, setQa] = useState()
+  const [answer, setAnswer] = useState()
 
-  // return (
-  //   <Swiper modules={[EffectFade]} effect="fade">
-  //     {[1, 2, 3].map((i, el) => {
-  //       return <SwiperSlide key={i}>Slide {el}</SwiperSlide>;
-  //     })}
-  //   </Swiper>
-  // );
+  useEffect(() => {
+    fetch("http://localhost:1200/v1/user/quiz/61fc06d724f2a6f6d98c1326")
+     .then(resp => resp.json())
+     .then(data => {
+      setQuiz(data.quizDescription)
+      setQa(data.questions)
+     })
+    }, [])
+  
   const onSlideEvent = (e) => {
-    console.log("lenght is:", mhaQna.length)
-    console.log("swiper on slider event : ", e.realIndex  )
-    if(mhaQna.length === e.realIndex+1){
-      console.log("at the last event>>>>")
+    console.log("lenght is:", mhaQna.length);
+    console.log("swiper on slider event : ", e.realIndex);
+    if (mhaQna.length === e.realIndex + 1) {
+      console.log("at the last event>>>>");
     }
-  }
+  };
 
   return (
     <div className="App">
       <Nav />
-     
+
       <div className="qnaContainer">
         {/* <AnimeInfo /> */}
         <Swiper
           direction="vertical"
-          slidesPerView={'auto'}
+          slidesPerView={"auto"}
           // modules={[Pagination, Scrollbar, A11y]}
           pagination={{ clickable: true }}
           spaceBetween={30}
           onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={(e)=>onSlideEvent(e)}
+          onSlideChange={(e) => onSlideEvent(e)}
           navigationk
           scrollbar={{ draggable: true }}
         >
           <SwiperSlide>
-          <AnimeInfo />
-          </SwiperSlide >
-          {
-            mhaQna.map((qna, index) => {
-              return (
-                <SwiperSlide key={index}>
-                  <Qna
-                    question={qna.question}
-                    options={qna.options}
-                    questionIndex = {index}
-                    totalQuestion = {mhaQna.length}
-                  />
-                </SwiperSlide>
-              )
-            })
-
-          }
-        <SwiperSlide>
-        <div className="btnContainer">
-        <Button variant="contained" size="large" style={{ background: "green", width: "auto" }}>
-          Submite Quiz
-        </Button>
-        <Button variant="contained" size="large" style={{ background: "gray", width: "auto" }}>
-          reset
-        </Button>
-      </div>
-        </SwiperSlide>
-
+            <AnimeInfo />
+          </SwiperSlide>
+          {qa?.map((qna, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <Qna
+                  id= {qna._id}
+                  question={qna.question}
+                  options={qna.options}
+                  questionIndex={index}
+                  totalQuestion={qa.length}
+                />
+              </SwiperSlide>
+            );
+          })}
+          <SwiperSlide>
+            <div className="btnContainer">
+              <Button
+                variant="contained"
+                size="large"
+                style={{ background: "green", width: "auto" }}
+              >
+                Submite Quiz
+              </Button>
+              <Button
+                variant="contained"
+                size="large"
+                style={{ background: "gray", width: "auto" }}
+              >
+                reset
+              </Button>
+            </div>
+          </SwiperSlide>
         </Swiper>
       </div>
-      
-
     </div>
   );
 }
--- src/components/Qna.js
+++ src/components/Qna.js
@@ -2,6 +2,11 @@ import React from 'react';
 import "./Qna.css"
 
 function Qna(props) {
+  const handleOptionSubmit = (questionId, e) => {
+    console.log("u clicked Me.", questionId)
+    console.log("u clicked Meeeeee.", e.target)
+  }
+
   return <div className='qna'>
     <div className='headerQna'>
       <span>
@@ -10,11 +15,11 @@ function Qna(props) {
         <span>{props.totalQuestion}</span>
       </span>
     </div>
-    <div id='question'>{props.question}</div>
+    <div id="question">{props.question}</div>
     <div className='optionContainer'>
     {
       props.options.map((option, index) => {
-        return <div key={index} className='option'>
+        return <div key={index} onClick={(e) => {handleOptionSubmit(props.id, e)}} className='option'>
           {option}
           </div>
       })
@@ -30,7 +35,6 @@ Qna.defaultProps = {
     "this is option B",
     "this is option C",
     "this is option D"
-
   ]
 }
 
